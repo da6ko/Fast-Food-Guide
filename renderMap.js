ba4c4a09265cc747a757509a8dd0cd95cfdde15f
da6ko <kmarchev00@gmail.com>
2024-01-11 17:55:03 +0200

    MongoDB server

--- renderMap.js
+++ renderMap.js
@@ -1,26 +1,23 @@
 mapboxgl.accessToken = 'YOUR-MAPBOXGL-TOKEN';
 const map = new mapboxgl.Map({
-container: 'map',
-center: [25.238569, 42.766279], 
-zoom: 7
+  container: 'map',
+  center: [25.238569, 42.766279],
+  zoom: 7
 });
 
+fetch('http://localhost:3000/markers')
+  .then(response => response.json())
+  .then(markers => {
+    markers.forEach(marker => {
+      const popupContent = `
+        <h3>${marker.title}</h3>
+        <p>Rating: ${marker.rating} stars</p>
+      `;
 
-const markers = [
-    { coordinates: [22.874792, 43.988458], title: 'Густото', rating: 4 },
-    { coordinates: [23.335326, 42.697960], title: 'Дюнерландия', rating: 5 },
-    { coordinates: [27.471433, 42.493436], title: 'Iskender Kebap', rating: 3.5},
-    { coordinates: [25.314915, 42.870864], title: 'Дон Густо', rating: 2 }
-  ];
-
-  markers.forEach(marker => {
-    const popupContent = `
-    <h3>${marker.title}</h3>
-    <p>Rating: ${marker.rating} stars</p>
-  `;
-
-    new mapboxgl.Marker()
-      .setLngLat(marker.coordinates)
-      .setPopup(new mapboxgl.Popup().setHTML(popupContent))
-      .addTo(map);
-  });
+      new mapboxgl.Marker()
+        .setLngLat(marker.coordinates)
+        .setPopup(new mapboxgl.Popup().setHTML(popupContent))
+        .addTo(map);
+    });
+  })
+  .catch(error => console.error(error));
