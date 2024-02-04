ebcf0fa419756aafb36cd0a2b8bb7dd3cab39ebd
da6ko <kmarchev00@gmail.com>
2024-02-04 14:55:56 +0200

    working database

--- renderMap.js
+++ renderMap.js
@@ -1,23 +1,29 @@
-mapboxgl.accessToken = 'YOUR-MAPBOXGL-TOKEN';
+mapboxgl.accessToken = 'YOUR_MAPBOX_TOKEN';
 const map = new mapboxgl.Map({
   container: 'map',
   center: [25.238569, 42.766279],
   zoom: 7
 });
 
-fetch('http://localhost:3000/markers')
-  .then(response => response.json())
-  .then(markers => {
-    markers.forEach(marker => {
-      const popupContent = `
-        <h3>${marker.title}</h3>
-        <p>Rating: ${marker.rating} stars</p>
-      `;
+map.on('load', () => {
+  const backendURL = 'http://localhost:8080/api/get-feedback';
+  console.log('Fetching data from:', backendURL);
+  console.log(backendURL);
+  fetch(backendURL)
+    .then(response => response.json())
+    .then(markers => {
+      markers.forEach(marker => {
+        const popupContent = `
+          <h3>${marker.name}</h3>
+          <p>Rating: ${marker.rate} stars</p>
+          <p>Comment: ${marker.comment}</p>
+        `;
 
-      new mapboxgl.Marker()
-        .setLngLat(marker.coordinates)
-        .setPopup(new mapboxgl.Popup().setHTML(popupContent))
-        .addTo(map);
-    });
-  })
-  .catch(error => console.error(error));
+        new mapboxgl.Marker()
+          .setLngLat([parseFloat(marker.longitude), parseFloat(marker.latitude)])
+          .setPopup(new mapboxgl.Popup().setHTML(popupContent))
+          .addTo(map);
+      });
+    })
+    .catch(error => console.error(error));
+});
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const MONGO_URI = 'YOUR_MONGODB_URI/DATABASE_NAME';
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const feedbackSchema = new mongoose.Schema({
+  name: String,
+  rate: Number,
+  comment: String,
+  latitude: Number,
+  longitude: Number,
+});
+
+const Feedback = mongoose.model('Feedback', feedbackSchema, 'feedbackCollection');
+
+
+app.get('/api/get-feedback', async (req, res) => {
+  try {
+    const allFeedback = await Feedback.find({}, { _id: 0, name: 1, rate: 1, comment: 1, latitude: 1, longitude: 1 });
+    res.json(allFeedback);
+  } catch (error) {
+    console.error('Error fetching feedback:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
